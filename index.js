2703662b2c73f923a1aab329f147db2eb4bab49d
EstherMutua <esthermutua16@gmail.com>
2025-02-28 22:32:36 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
-}
+    const button = document.getElementById("button"); // Updated to match the correct ID
+  
+    if (button) {
+      button.addEventListener("click", () => {
+        console.log("Button clicked! Event listener added.");
+      });
+    }
+  }
+  
